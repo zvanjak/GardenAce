ba480f252fa78269a153beefb0feb5639fa77921
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2022-04-06 00:51:08 +0200

    ideje

--- GardenAce.Domain/Estate.cs
+++ GardenAce.Domain/Estate.cs
@@ -5,5 +5,11 @@ namespace GardenAce.Domain
     public class Estate
     {
       // point of ishodište - di ćemo u lokalnom staviti 0,0
+
+      // ima lokalno definiran koord. sustav!!! u ODNOSU na ishodište
+      // sustav je 3D, ali je lokalna ravnina
+      // orijentacija je sjever-jug, nema diskusije
+
+    // trebati će mi lokalna transformacija!
     }
 }
