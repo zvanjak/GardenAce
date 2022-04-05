6b11fb6a25d6099e2e8b81a0d8d561e20936287c
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2022-04-06 01:39:38 +0200

    tot je to

---
+++ GardenAce.Domain/EstatePart.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GardenAce.Domain
+{
+  internal class EstatePart
+  {
+    // osnovno - zauzima dio tlocrta od estate!
+  }
+}
