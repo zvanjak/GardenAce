936d620198437555fd4b190ce1ab9047f31a1adc
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2022-04-06 00:25:16 +0200

    a

--- TestGeoCoord/Program.cs
+++ TestGeoCoord/Program.cs
@@ -10,6 +10,17 @@ namespace TestGeoCoord
   {
     static void Main(string[] args)
     {
+      double x1 = 45.73779885083265;
+      double y1 = 15.936093764979713;
+
+      double x2 = 45.73775302153398;
+      double y2 = 15.93626284812708;
+
+      double x3 = 45.735977758275176;
+      double y3 = 15.93523577830417;
+
+      double x4 = 45.735962290389196;
+      double y4 = 15.935010881339498;
     }
   }
 }
