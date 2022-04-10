fd92933320867297175b80261eace49a50b6284b
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2022-04-10 20:49:03 +0200

    ide

--- EstateManager.Domain/EstateLegalGeoDefinition.cs
+++ EstateManager.Domain/EstateLegalGeoDefinition.cs
@@ -6,12 +6,23 @@ using System.Threading.Tasks;
 
 namespace EstateManager.Domain
 {
+
+  // ima lokalno definiran koord. sustav!!! u ODNOSU na ishodište
+  // sustav je 3D, ali je lokalna ravnina
+  // orijentacija je sjever-jug, nema diskusije
+  //  U STVARI NE - OĆU LOKALNO LIJEPE KOORDINATE,  dakle, zadajem orijentaciju da mi y bude točno po ogradi!
+
+  // trebati će mi lokalna transformacija!
+
   public class EstateLegalGeoDefinition
   {
     // ima Geo Polyline za definiciju granice
     private List<GeoCoord> borderPoints = new List<GeoCoord>();
 
+    // point of ishodište - di ćemo u lokalnom staviti 0,0
     GeoCoord _localOrigin;
+    
+    // i koliko je zarotiran
     double _localRotation;
 
     public List<GeoCoord> BorderPoints { get => borderPoints; set => borderPoints = value; }
