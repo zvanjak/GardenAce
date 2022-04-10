ab36e33ae53c725f35e20c61996246cb229850fd
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2022-04-10 17:18:04 +0200

    malo

--- EstateManager.Domain/EstateLegalGeoDefinition.cs
+++ EstateManager.Domain/EstateLegalGeoDefinition.cs
@@ -11,6 +11,9 @@ namespace EstateManager.Domain
     // ima Geo Polyline za definiciju granice
     private List<GeoCoord> borderPoints = new List<GeoCoord>();
 
+    GeoCoord _localOrigin;
+    double _localRotation;
+
     public List<GeoCoord> BorderPoints { get => borderPoints; set => borderPoints = value; }
 
     public EstateLegalGeoDefinition()
@@ -19,6 +22,25 @@ namespace EstateManager.Domain
       BorderPoints.Add(new GeoCoord() { Latitude = 45.73775302153398, Longitude = 15.93626284812708 });
       BorderPoints.Add(new GeoCoord() { Latitude = 45.735977758275176, Longitude = 15.93523577830417 });
       BorderPoints.Add(new GeoCoord() { Latitude = 45.735962290389196, Longitude = 15.935010881339498 });
+    }
+
+    public void LocalCoord(GeoCoord origin, GeoCoord pnt, out double x, out double y)
+    {
+      GeoCoord sameLat = new GeoCoord() { Latitude = _localOrigin.Latitude, Longitude = pnt.Longitude };
+      GeoCoord sameLon = new GeoCoord() { Latitude = pnt.Latitude, Longitude = _localOrigin.Longitude };
+
+      x = Transformations.Distance(_localOrigin, sameLat);
+      if (pnt.Longitude < _localOrigin.Longitude)
+        x *= -1;
+
+      y = Transformations.Distance(_localOrigin, sameLon);
+      if (pnt.Latitude < _localOrigin.Latitude)
+        y *= -1;
+
+      // još rotacija
+      double phi = 19.9055 * 3.14159 / 180;
+      x = x * Math.Cos(phi) - y * Math.Sin(phi);
+      y = x * Math.Sin(phi) + y * Math.Cos(phi);
     }
   }
 }
