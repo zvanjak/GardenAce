6938d4060c9f2c3ee1a44afc0a17d63a56e6539b
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2022-04-10 21:23:56 +0200

    malo

--- EstateManager.Domain/Estate.cs
+++ EstateManager.Domain/Estate.cs
@@ -12,6 +12,21 @@ namespace EstateManager.Domain
 
     public List<EstatePart> _estateParts = new List<EstatePart>();
 
+    public Estate()
+    {
+      LegalGeoDefinition = new EstateLegalGeoDefinition();
+
+      LegalGeoDefinition.BorderPoints.Add(new GeoCoord() { Latitude = 45.73779885083265, Longitude = 15.936093764979713 });
+      LegalGeoDefinition.BorderPoints.Add(new GeoCoord() { Latitude = 45.73775302153398, Longitude = 15.93626284812708 });
+      LegalGeoDefinition.BorderPoints.Add(new GeoCoord() { Latitude = 45.735977758275176, Longitude = 15.93523577830417 });
+      LegalGeoDefinition.BorderPoints.Add(new GeoCoord() { Latitude = 45.735962290389196, Longitude = 15.935010881339498 });
+
+      LegalGeoDefinition._localOrigin = LegalGeoDefinition.BorderPoints[1];
+      LegalGeoDefinition._localRotation = 19.9055 * 3.14159 / 180;
+
+      // svi su u DonjiVrt
+      // dodati garden plot za krumpire, kapulu, salatu i jagode
+    }
     EstatePart getAtLocation(PointF pnt)
     {
       return null;
--- EstateManager.Domain/EstateLegalGeoDefinition.cs
+++ EstateManager.Domain/EstateLegalGeoDefinition.cs
@@ -20,22 +20,15 @@ namespace EstateManager.Domain
     private List<GeoCoord> borderPoints = new List<GeoCoord>();
 
     // point of ishodište - di ćemo u lokalnom staviti 0,0
-    GeoCoord _localOrigin;
+    public GeoCoord _localOrigin;
     
     // i koliko je zarotiran
-    double _localRotation;
+    public double _localRotation;
 
     public List<GeoCoord> BorderPoints { get => borderPoints; set => borderPoints = value; }
 
     public EstateLegalGeoDefinition()
     {
-      BorderPoints.Add(new GeoCoord() { Latitude = 45.73779885083265, Longitude = 15.936093764979713 });
-      BorderPoints.Add(new GeoCoord() { Latitude = 45.73775302153398, Longitude = 15.93626284812708 });
-      BorderPoints.Add(new GeoCoord() { Latitude = 45.735977758275176, Longitude = 15.93523577830417 });
-      BorderPoints.Add(new GeoCoord() { Latitude = 45.735962290389196, Longitude = 15.935010881339498 });
-
-      _localOrigin = BorderPoints[1];
-      _localRotation = 19.9055 * 3.14159 / 180;
     }
 
     public void LocalCoord(GeoCoord pnt, out double x, out double y)
