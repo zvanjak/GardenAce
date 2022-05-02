1a747eca4abd882c01039f1b07c2e6e81c0da4c7
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2022-05-03 00:16:52 +0200

    ide

--- EstateManager.Domain/Estate.cs
+++ EstateManager.Domain/Estate.cs
@@ -8,29 +8,34 @@ namespace EstateManager.Domain
 {
   public class Estate
   {
-    string _name;   // Dupci, Sukošan, Silba
-    public EstateGeoDefinition _geoDefinition { get; set; }
+    private string _name;   // Dupci, Sukošan, Silba
+    private EstateGeoDefinition _geoDefinition;
+    private List<EstatePart> _estateParts = new List<EstatePart>();
 
-    public List<EstatePart> _estateParts = new List<EstatePart>();
+    public string Name { get => _name; set => _name = value; }
+    public EstateGeoDefinition GeoDefinition { get => _geoDefinition; set => _geoDefinition = value; }
+    public List<EstatePart> EstateParts { get => _estateParts; private set => _estateParts = value; }
+
 
     public Estate()
     {
       // default je Dupci estate
-      _geoDefinition = new EstateGeoDefinition();
+      GeoDefinition = new EstateGeoDefinition();
 
-      _geoDefinition.BorderPoints.Add(new GeoCoord() { Latitude = 45.73779885083265, Longitude = 15.936093764979713 });
-      _geoDefinition.BorderPoints.Add(new GeoCoord() { Latitude = 45.73775302153398, Longitude = 15.93626284812708 });
-      _geoDefinition.BorderPoints.Add(new GeoCoord() { Latitude = 45.735977758275176, Longitude = 15.93523577830417 });
-      _geoDefinition.BorderPoints.Add(new GeoCoord() { Latitude = 45.735962290389196, Longitude = 15.935010881339498 });
+      GeoDefinition.BorderPoints.Add(new GeoCoord() { Latitude = 45.73779885083265, Longitude = 15.936093764979713 });
+      GeoDefinition.BorderPoints.Add(new GeoCoord() { Latitude = 45.73775302153398, Longitude = 15.93626284812708 });
+      GeoDefinition.BorderPoints.Add(new GeoCoord() { Latitude = 45.735977758275176, Longitude = 15.93523577830417 });
+      GeoDefinition.BorderPoints.Add(new GeoCoord() { Latitude = 45.735962290389196, Longitude = 15.935010881339498 });
 
-      _geoDefinition._localOrigin = _geoDefinition.BorderPoints[1];
-      _geoDefinition._localRotation = 19.9055 * 3.14159 / 180;
+      GeoDefinition._localOrigin = GeoDefinition.BorderPoints[1];
+      GeoDefinition._localRotation = 19.9055 * 3.14159 / 180;
     }
 
     public Estate(EstateGeoDefinition inGeoDef)
     {
-      _geoDefinition = inGeoDef;
+      GeoDefinition = inGeoDef;
     }
+
     EstatePart getAtLocation(PointF pnt)
     {
       return null;
