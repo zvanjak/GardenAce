b8d8b36330f5cb05128ab477126c4de71695573f
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2022-05-03 00:07:28 +0200

    refactoring

--- EstateManager.Console/Program.cs
+++ EstateManager.Console/Program.cs
@@ -9,17 +9,25 @@ namespace EstateManager.ConsoleApp
   {
     static void Main(string[] args)
     {
-      EstateGeoDefinition def = new EstateGeoDefinition();
+      EstateGeoDefinition LegalGeoDefinition = new EstateGeoDefinition();
 
-      GeoCoord origin = def.BorderPoints[1];
+      LegalGeoDefinition.BorderPoints.Add(new GeoCoord() { Latitude = 45.73779885083265, Longitude = 15.936093764979713 });
+      LegalGeoDefinition.BorderPoints.Add(new GeoCoord() { Latitude = 45.73775302153398, Longitude = 15.93626284812708 });
+      LegalGeoDefinition.BorderPoints.Add(new GeoCoord() { Latitude = 45.735977758275176, Longitude = 15.93523577830417 });
+      LegalGeoDefinition.BorderPoints.Add(new GeoCoord() { Latitude = 45.735962290389196, Longitude = 15.935010881339498 });
+
+      LegalGeoDefinition._localOrigin = LegalGeoDefinition.BorderPoints[1];
+      LegalGeoDefinition._localRotation = 19.9055 * 3.14159 / 180;
+
+      GeoCoord origin = LegalGeoDefinition.BorderPoints[1];
 
       for( int i= 0; i < 4; i++ )
       {
         int j = (i + 1) % 4;
-        double dist = Transformations.Distance(def.BorderPoints[i], def.BorderPoints[j]);
+        double dist = Transformations.Distance(LegalGeoDefinition.BorderPoints[i], LegalGeoDefinition.BorderPoints[j]);
         Console.WriteLine("Dist {0}-{1} = {2}", i, j, dist);
 
-        GeoCoord pnt = def.BorderPoints[i];
+        GeoCoord pnt = LegalGeoDefinition.BorderPoints[i];
 
         double x, y;
         Transformations.LocalCoord(origin, pnt, out x, out y);
