87127b5b8f87633829a2d2733286dbc89add99b3
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2022-05-03 01:44:47 +0200

    ide

--- EstateManager.Domain/SeededGardenRow.cs
+++ EstateManager.Domain/SeededGardenRow.cs
@@ -9,18 +9,22 @@ namespace EstateManager.Domain
   public class SeededGardenRow
   {
     string _cultureName;
+    GardenPlot _parent;
     DateTime _dateSeeded;
 
     public string CultureName { get => _cultureName; set => _cultureName = value; }
     public DateTime DateSeeded { get => _dateSeeded; set => _dateSeeded = value; }
+    public GardenPlot Parent { get => _parent; set => _parent = value; }
+
     // koliko?
 
     // EVENTS?
 
-    public SeededGardenRow(string inName, DateTime inDateSeeded)
+    public SeededGardenRow(string inName, DateTime inDateSeeded, GardenPlot inParent)
     {
       _cultureName = inName;
       _dateSeeded = inDateSeeded;
+      _parent = inParent;
     }
   }
 }
